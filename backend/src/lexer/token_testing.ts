b06d470d46f5e539d78dc627eddba54a9b0e0363
Jeff Petterson Bernandino Mercado <141805107+Jettsky09@users.noreply.github.com>
2025-12-19 14:08:30 +0800

    Added pdf generator logic

---
+++ backend/src/lexer/token_testing.ts
@@ -0,0 +1,19 @@
+import { generatePDF } from "./pdf_generator";
+import { Token, TokenType } from "../types/Tokens";
+
+const mockTokens: Token[] = [
+  { lexeme: "scene", type: TokenType.K_Scene, line: 1, column: 1 },
+  { lexeme: "start", type: TokenType.K_Start, line: 2, column: 1 },
+  { lexeme: "x", type: TokenType.Identifier, line: 3, column: 5 },
+  { lexeme: "=", type: TokenType.OP_Assign, line: 3, column: 7 },
+  { lexeme: "10", type: TokenType.NumberLiteral, line: 3, column: 9 },
+  { lexeme: ";", type: TokenType.D_Semicolon, line: 3, column: 11 },
+  { lexeme: "scene", type: TokenType.K_Scene, line: 1, column: 1 },
+  { lexeme: "start", type: TokenType.K_Start, line: 2, column: 1 },
+  { lexeme: "x", type: TokenType.Identifier, line: 3, column: 5 },
+  { lexeme: "=", type: TokenType.OP_Assign, line: 3, column: 7 },
+  { lexeme: "10", type: TokenType.NumberLiteral, line: 3, column: 9 },
+  { lexeme: ";", type: TokenType.D_Semicolon, line: 3, column: 11 }
+];
+
+generatePDF(mockTokens, "test_tokens.pdf");
